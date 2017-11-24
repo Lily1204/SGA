fb4f7f67bc9ddd45ed185363e36192fe876df755
Damian Zamora <emer.dzc@gmail.com>
2017-11-23 21:14:22 -0600

    Actualizacion backend
    Nuevos archivos de back, actualizados a ingles

---
+++ Codigo/Backend/Config.php
@@ -0,0 +1,12 @@
+<?php 
+// Definiendo constantes con los datos de la BD 
+
+	define("LOCALHOST", "localhost");
+
+	define("USER", "root");
+
+	define("PASSWORD", "");
+
+	define("DATABASE", "sga");
+
+ ?>
---
+++ Codigo/Backend/Index.php
@@ -0,0 +1,5 @@
+<?php
+    require_once "StudentApi.php";// Llama a la clase studentApi    
+    $studentApi = new StudentApi();// Crea objeto 
+    $studentApi->Api();// Instancia el metodo api para envio de peticiones
+?>
